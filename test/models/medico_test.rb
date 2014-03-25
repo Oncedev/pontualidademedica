758ed648349370c8d54be2eead53bd95be0dc822
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-03-25 15:44:36 -0300

    Adicionar testes ao modelo dos Médicos

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MedicoTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "medico sem nada" do
+    medico = Medico.new
+    assert !medico.save, "Medico vazio"
+  end
+
+  test "medico sem nome" do
+    medico = Medico.new CRM: 1001
+    assert !medico.save, "Medico sem nome"
+  end
+
+  test "medico sem crm" do
+    medico = Medico.new nome: "Alberto Costa"
+    assert !medico.save, "Medico sem CRM"
   end
 end
