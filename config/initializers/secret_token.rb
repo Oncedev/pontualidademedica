7d019645e6c5c93b93692470e043d64413851238
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-03-25 15:09:56 -0300

    Create rails template app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ponutalidademedica::Application.config.secret_key_base = 'cf0eef3c9bdb5225a28d62fdf69e1814b93ef10073a6cff6a7735991a42aa44cb075807082d497ae23c905ec4c24e07a271dd16cd3e8c2e7493907c943b3468b'
