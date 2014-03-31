08b5c115d5d594c61df50568e101d1b1082a7ed5
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-03-31 13:31:38 -0300

    Cadastro de consulta

--- app/controllers/consultas_controller.rb
+++ app/controllers/consultas_controller.rb
@@ -1,3 +1,12 @@
+# http://jeffgardner.org/2011/08/04/rails-string-to-boolean-method/
+class String
+  def to_bool
+    return true if self == true || self =~ (/(true|t|yes|y|1)$/i)
+    return false if self == false || self.blank? || self =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+  end
+end
+
 class ConsultasController < ApplicationController
   before_action :set_consulta, only: [:show, :edit, :update, :destroy]
 
@@ -14,7 +23,14 @@ class ConsultasController < ApplicationController
 
   # GET /consultas/new
   def new
-    @consulta = Consulta.new
+    flash[:errors] = []
+
+    if session[:usuario].nil?
+      flash[:errors] = ["Você precisa logar para adicionar uma nova consulta"]
+      redirect_to controller: "usuarios", action: "login"
+    else
+      @consulta = Consulta.new usuario_id: session[:usuario]
+    end
   end
 
   # GET /consultas/1/edit
@@ -24,13 +40,44 @@ class ConsultasController < ApplicationController
   # POST /consultas
   # POST /consultas.json
   def create
-    @consulta = Consulta.new(consulta_params)
+    flash[:errors] = []
+    par = consulta_params
+
+    medico = Medico.find_by CRM: params[:CRM_medico].to_i
+    if medico.nil?
+      medico = Medico.new nome: params[:nome_medico], CRM: params[:CRM_medico].to_i
+      medico_ok = medico.save
+      flash[:errors] += medico.errors.full_messages
+    end
+
+    @consulta = Consulta.new(
+      usuario_id: session[:usuario].id,
+      medico_id: medico.nil? ? nil : medico.id,
+      anonimo: par[:anonimo].to_bool,
+      hora_marcacao: DateTime.new(
+        par["hora_marcacao(1i)"].to_i,
+        par["hora_marcacao(2i)"].to_i,
+        par["hora_marcacao(3i)"].to_i,
+        par["hora_marcacao(4i)"].to_i,
+        par["hora_marcacao(5i)"].to_i,
+        0
+      ),
+      hora_atendimento: DateTime.new(
+        par["hora_atendimento(1i)"].to_i,
+        par["hora_atendimento(2i)"].to_i,
+        par["hora_atendimento(3i)"].to_i,
+        par["hora_atendimento(4i)"].to_i,
+        par["hora_atendimento(5i)"].to_i,
+        0
+      )
+    )
 
     respond_to do |format|
       if @consulta.save
-        format.html { redirect_to @consulta, notice: 'Consulta was successfully created.' }
+        format.html { redirect_to controller: "medicos", notice: "Consulta registrada" }
         format.json { render action: 'show', status: :created, location: @consulta }
       else
+        flash[:errors] += @consulta.errors.full_messages
         format.html { render action: 'new' }
         format.json { render json: @consulta.errors, status: :unprocessable_entity }
       end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,6 +1,6 @@
 class Usuario < ActiveRecord::Base
   validates :nome, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :senha, presence: true
   has_many :consultas
 end
