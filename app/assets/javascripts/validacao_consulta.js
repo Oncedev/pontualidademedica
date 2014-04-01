4a2437e0d25874431a64651ec89935a79a7c670f
Valdiney França <valdiney.2@hotmail.com>
2014-04-01 17:51:46 -0300

    cadastro
    arrumando cadastro

---
+++ app/assets/javascripts/validacao_consulta.js
@@ -0,0 +1,22 @@
+$(document).ready( function() {
+
+   $('#button_registrar').click( function() {
+     
+     var nome = $('#nome'),
+         crm = $('.crm'),
+         data = $('.date_picker'),
+         horaMarcacaoEatendimento = $('.time_field');
+
+       /*
+         if ( nome.val() == '' crm.val() == '' ) {
+         	alert('em branco');
+         	return false;
+         }
+*/
+
+         
+
+   });
+
+
+});
