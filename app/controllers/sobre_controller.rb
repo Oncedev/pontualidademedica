b8dbe863c761e675c1a992163f72c893c3cea282
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-04-03 16:50:03 -0300

    Mensagem de sobre

--- app/controllers/sobre_controller.rb
+++ app/controllers/sobre_controller.rb
@@ -1,8 +1,17 @@
 class SobreController < ApplicationController
   def index
-    @titulo = "Pontualidade médica"
+    @titulo = "O que é Pontualidade Médica?"
     @paragrafos = [
-      "Pontualidade médica é isso aí"
+      "\"Pontualidade Médica\" permite o registro de ocorrências de atraso " +
+      "de atendimento por parte dos médicos pelos pacientes.",
+      "A página inicial exibe uma lista de médicos ordenados por tempo médio de " +
+      "de atraso das consultas reclamadas." +
+      "Para registrar uma ocorrência de atraso, basta criar uma conta e " +
+      "reclamar. Informe o nome e o CRM do médico, data da consulta, hora " +
+      "marcada, hora em que você foi atendido e pronto: a reclamação estará " +
+      "registrada e o médico, se já não estava na lista, aparecerá nela.",
+      "Esta ainda é uma versão preliminar de testes do \"Pontualidade Médica\", " +
+      "então algumas coisas podem estar faltando ou não funcionar corretamente."
     ]
   end
 end
