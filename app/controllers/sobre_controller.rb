e0f387e81124f8aa561702e966b9e848c6bb0b0f
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-04-04 08:31:24 -0300

    Atualizar mensagem de sobre

--- app/controllers/sobre_controller.rb
+++ app/controllers/sobre_controller.rb
@@ -5,13 +5,13 @@ class SobreController < ApplicationController
       "\"Pontualidade Médica\" permite o registro de ocorrências de atraso " +
       "de atendimento por parte dos médicos pelos pacientes.",
       "A página inicial exibe uma lista de médicos ordenados por tempo médio de " +
-      "de atraso das consultas reclamadas." +
+      "atraso das consultas reclamadas." +
       "Para registrar uma ocorrência de atraso, basta criar uma conta e " +
       "reclamar. Informe o nome e o CRM do médico, data da consulta, hora " +
       "marcada, hora em que você foi atendido e pronto: a reclamação estará " +
       "registrada e o médico, se já não estava na lista, aparecerá nela.",
-      "Esta ainda é uma versão preliminar de testes do \"Pontualidade Médica\", " +
-      "então algumas coisas podem estar faltando ou não funcionar corretamente."
+      "Esta versão do \"Pontualidade Médica\" é uma versão Beta. Muitas melhorias " +
+      "e recursos novos ainda estão por vir."
     ]
   end
 end
