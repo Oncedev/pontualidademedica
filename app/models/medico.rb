953b351fee0ea985955554d9697e53952fcbd75d
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-04-07 14:23:28 -0300

    Deixa passar se o CRM estiver vazio

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,6 +1,6 @@
 class ApenasNumerosValidator < ActiveModel::EachValidator
   def validate_each(medico, attr, val)
-    unless /^[0-9]+$/ =~ val.to_s
+    unless /^[0-9]*$/ =~ val.to_s
       medico.errors[attr] << "não pode conter letras"
     end
   end
