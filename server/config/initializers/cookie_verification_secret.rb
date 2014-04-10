bfdabdd2689f2e4ba8f49227e804486780468824
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-04-10 09:20:12 -0300

    Remover aplicações rails que entraram por engano

--- server/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f6cf18aaa6662b4dbd9367975bc1503868438b67b6401af8fa24b5812fd2c51af305b9ff8e60ae198fe6fcbb7c86a870cbc3015938949c81cecaf3bb3a597045';
