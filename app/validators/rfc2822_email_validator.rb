4350560bfd8112bb44f94a27ffefcf46b4ec843e
Luiz Romário Santana Rios <luizromario@gmail.com>
2014-04-10 09:46:26 -0300

    Tirar string vazia do validador de email

--- app/validators/rfc2822_email_validator.rb
+++ app/validators/rfc2822_email_validator.rb
@@ -8,7 +8,7 @@ class Rfc2822EmailValidator < ActiveModel::EachValidator
   private
   def valid_email( value )
     begin
-      return false if value == ''
+      return true if value == ''
       parsed = Mail::Address.new( value )
       return parsed.address == value && parsed.local != parsed.address
     rescue Mail::Field::ParseError
